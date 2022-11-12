b885f1a4a07ce80c785abb74d5a88e49d4c6b65d
Lukas Lindner <ll@MARS-SOLUTIONS.loc>
2022-11-12 15:04:02 +0100

    Init

---
+++ code/holeDatenquelle.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+"""Skript für den Download und die Filterung des deutschsprachige Wörterbuch von
+https://sourceforge.net/projects/germandict/"""
+
+lizenz = \
+"Dieser Quellkode steht unter der GNU GENERAL PUBLIC LICENSE 3 oder neuer"
+skriptName = "holeDatenquelle.py"
+skriptVersion = "1"
+skriptDatum = "2022-10-08"
+autor = "Lukas Lindner"
+skriptId = \
+f"{skriptName} {skriptVersion}, {skriptDatum}, Copyright (c) {autor}"
+aufrufKurz = f"Aufruf:    {skriptName}"
+copyright = """Dieser Skript ist freie Software, die Sie unter bestimmten
+Bedingungen weitergeben dürfen. Diese stehen im Quellkode.
+Für dieses Python-Skript besteht KEINERLEI GARANTIE, weder für MARKTREIFE 
+noch für eine VERWENDBARKEIT FÜR EINEN BESTIMMTEN ZWECK."""
+
+aufrufLang = \
+f"""{skriptName} lädt die Wortliste von
+https://sourceforge.net/projects/germandict/ herunter und speichert eine auf
+Anzahl der Zeichen gefilterte Liste in eine UTF-8 Datei. 
+
+Nach der Eingabe der Anzahl Buchstaben als Filterkriterium gibt {skriptName}
+entweder eine Erfolgsmeldung inkl. Anzahl der Wörter oder eine Fehlermeldung
+aus.
+
+Beispielhafter Aufruf:   {skriptName}
+
+Es gibt keine Optionen für den Aufruf""" # aufrufLang
+
+def druckeSkriptId():
+    print(skriptId)
+    return None
+
+def druckeAufrufKurz():
+    druckeSkriptId()
+    print()
+    print(aufrufKurz)
+    return None
+
+def druckeAufrufLang():
+    druckeSkriptId()
+    print(copyright)
+    print()
+    print(aufrufLang)
+    return None
+###############################################################################
+
+druckeSkriptId()
+# druckeAufrufKurz()
+# druckeAufrufLang()
+import os
+import glob
+import requests
+import pyunpack
+import csv
+import shutil
+
+
+# Wortlänge, falls 0 wird der Benutzer nach Vorgabe gefragt
+wortLaenge = 0
+#debug = False
+debug = True
+
+woerter = []
+
+# Download  und Import des Wörterbuchs germandict von ourceforge.net
+link = "https://sourceforge.net/projects/germandict/files/latest/download"
+skriptPfad = os.path.dirname(__file__)
+downloadPfad = os.path.join(skriptPfad, "german.7z")
+zielpfadArchiv = os.path.join(skriptPfad, "german-dict")
+woerterbuchPfad = os.path.join(zielpfadArchiv, "german.dic")
+ausgabeDatei = os.path.join(skriptPfad, "words.txt")
+
+istVorhanden = os.path.exists(downloadPfad)
+if not istVorhanden:
+    response = requests.get(link, stream=True)
+
+    if response.status_code == 200:
+        if debug:
+            print("Herunterladen erfolgreich")
+            printt(f"HTTP Status Code: {response.status_code}")
+        with open(downloadPfad, 'wb') as out:
+            out.write(response.content)
+    else:
+        print("Herunterladen fehlgeschlagen")
+        print(f"HTTP Status Code: {response.status_code}")
+
+istVorhanden = os.path.exists(zielpfadArchiv)
+if not istVorhanden:
+  os.makedirs(zielpfadArchiv)
+istVorhanden = os.path.exists(woerterbuchPfad)
+if not istVorhanden:
+    pyunpack.Archive(downloadPfad).extractall(zielpfadArchiv)
+
+if wortLaenge == 0:
+    print("Keine Wortlänge definiert, bitte die gewünschte Wortlänge angeben!")
+    wortLaenge = int(input("Wortlänge als Ganzzahl: "))
+    print(f"Es wurde Wörter mit {wortLaenge} Buchstaben gewählt")
+
+zaehler = 0
+with open(woerterbuchPfad, 'r') as fd:
+    reader = csv.reader(fd)
+    for row in reader:
+        if len(str(row[0])) == wortLaenge:
+            woerter.append(row[0])
+            zaehler += 1
+if debug:
+    print(f"Wörterbuch: {woerterbuchPfad}")
+    
+datei = open(ausgabeDatei, 'w')
+for word in woerter:
+    datei.write(word)
+datei.close()
+
+print(f"""{zaehler} Wörter mit {wortLaenge} Buchstaben aus dem Wörterbuch 
+germandict in folgende Ausgabedatei geschrieben: 
+{ausgabeDatei}""")
+
+if debug:
+    print(f"""Lösche temporäre Daten:
+    {downloadPfad}
+    {zielpfadArchiv}""")
+os.remove(downloadPfad)
+shutil.rmtree(zielpfadArchiv)
